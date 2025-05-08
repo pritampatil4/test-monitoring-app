0a8decf96269f7b43637770a5e2a63ce4d27c5df
Pritam Patil <ppritam821@gmail.com>
2025-05-08 18:36:22 +0530

    Specific API response and UI text comparison
    - Made API call and capture label values
    - Capture UI text
    - Verify fn written to check API data with UI data
    - changes made to open page once and execute all tests

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -39,6 +39,22 @@ class HomePage {
     return this.page.locator('xpath=//h2[text()="All latency"]');
   }
 
+  get filterRegion() {
+    return this.page.locator(
+      '//label[contains(@data-testid, "region")]/following-sibling::div//div[text()="All"]'
+    );
+  }
+
+  get buttonRefresh() {
+    return this.page.getByTestId("data-testid RefreshPicker run button");
+  }
+
+  get rowAllChecks() {
+    return this.page.locator(
+      'xpath=[data-testid="data-testid Panel header All checks"] [role="table"] [data-testid="data-testid table body"] [role="row"]'
+    );
+  }
+
   //actions methods
   async goToHome() {
     await this.page.goto("/a/grafana-synthetic-monitoring-app/home");
@@ -54,6 +70,66 @@ class HomePage {
     await expect(this.labelAllCheckErrorPercent).toBeVisible();
     await expect(this.labelAllLatency).toBeVisible();
   }
-}
 
+  async verifyFilterRegion() {
+    await expect(this.filterRegion).toBeVisible();
+    await this.filterRegion.click({ force: true });
+    await this.filterRegion.type("EMEA");
+    await this.page.keyboard.press("Enter");
+    let currentUrl = await this.page.url();
+    expect(currentUrl).toContain("region=EMEA");
+  }
+
+  async clickRefresh() {
+    await this.buttonRefresh.click();
+  }
+
+  async captureAPIResponseMixedLatencyDenom(page) {
+    await this.clickRefresh();
+    const requestUrlRegex = /mixed-0-/;
+    const specificMixedResponse = await page.waitForResponse(
+      (resp) => requestUrlRegex.test(resp.url()) && resp.status() === 200
+    );
+    const responseBody = await specificMixedResponse.json();
+
+    // const state = responseBody.results.state;
+
+    const { check_name, instance, job } =
+      responseBody.results["latency denom"].frames[0].schema.fields[1].labels;
+    return { check_name, instance, job };
+  }
+
+  async verifyAPIResponseMixedLatencyDenomLabelsOnUI() {
+    let APIresult = await this.captureAPIResponseMixedLatencyDenom(this.page);
+    let UIresult = await this.verifyAllCells();
+    expect(UIresult.textDNS).toContain(APIresult.check_name);
+    expect(UIresult.textGrafanaDotCom).toContain(APIresult.instance);
+    expect(UIresult.textHostnameDnsResolution).toContain(APIresult.job);
+    return { APIresult, UIresult };
+  }
+
+  async verifyAllCells() {
+    const tableBody = this.page.locator('[data-testid="data-testid table body"] [role="row"]');
+    const allCells = tableBody.locator('div[role="cell"]');
+
+    const cellsWithDns = allCells.filter({has: this.page.getByText("dns", { exact: true })});
+    expect(cellsWithDns).toBeVisible();
+    let textDNS = await cellsWithDns.textContent();
+
+    const cellsWithGrafanaDotCom = allCells.first().filter({ has: this.page.getByText("grafana.com", { exact: true }) });
+    expect(cellsWithGrafanaDotCom).toBeVisible();
+    let textGrafanaDotCom = await cellsWithGrafanaDotCom.textContent();
+
+    const cellsWithHostnameDnsResolution = allCells.filter({has: this.page.getByText("Grafana Main Hostname DNS Resolution", {exact: true,}),
+    });
+    expect(cellsWithHostnameDnsResolution).toBeVisible();
+    let textHostnameDnsResolution = await cellsWithHostnameDnsResolution.textContent();
+
+    return {
+      textDNS,
+      textGrafanaDotCom,
+      textHostnameDnsResolution,
+    };
+  }
+}
 export default HomePage;
--- tests/home.spec.js
+++ tests/home.spec.js
@@ -5,13 +5,34 @@ import HomePage from "../pages/HomePage";
 
 test.describe("Home Page Tests", () => {
   let homePage;
+  let page;
 
-  test.beforeEach(async ({ page }) => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
     homePage = new HomePage(page);
+    await homePage.goToHome();
+    await page.waitForTimeout(5000);
   });
 
-  test("verify home page labels", async ({ page }) => {
-    await homePage.goToHome();
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test.skip("verify home page labels", async () => {
     await homePage.verifyHomePagelabels();
+  });
+
+  test.skip("verify region filter", async () => {
+    await homePage.verifyFilterRegion();
+  });
+
+  test("verify API data with UI data", async () => {
+    await homePage.clickRefresh();
+    let allData = await homePage.verifyAPIResponseMixedLatencyDenomLabelsOnUI();
+    console.log("All data ---->", allData);
+    await page.waitForTimeout(2000);
+    // await homePage.verifyAllCells();
+
+
   });
 });
